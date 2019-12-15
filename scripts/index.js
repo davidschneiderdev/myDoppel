224e1f00c67fbb86e11fdd26eeea3f697148d77d
David Schneider <david.jay.schneider@gmail.com>
2019-12-15 13:08:18 -0500

    adds new Character Object to function

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,45 @@
 
-let searchItem = 'portraits';
-const APIaddress = `https://collectionapi.metmuseum.org/public/collection/v1/search?q=${searchItem}`;
+const theOfficeCast = 'http://api.tvmaze.com/shows/526/cast';
+const gameOfThronesCast = 'http://api.tvmaze.com/shows/82/cast';
+const walkingDeadCast = 'http://api.tvmaze.com/shows/73/cast';
+const referenceImage = document.getElementById('referenceImage');
 
-function getObjectArray(APIaddress) {
-    return fetch(APIaddress)
-            .then(r => r.json())
-            .then(result => { return result.objectIDs.slice(0, 50) } )
+function loadModel() {
+    const mainModel = faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
+    const landmarkModel = faceapi.nets.faceLandmark68Net.loadFromUri('/models')
+    const recModel = faceapi.nets.faceRecognitionNet.loadFromUri('/models')
+    // await faceapi.nets.faceLandmark68TinyNet.loadFromUri('/models'),
+    // await faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    return Promise.all([mainModel, landmarkModel, recModel])
 }
 
-function getImage(objectID) {
-    let ObjectAddress = `https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectID}`
-    fetch(ObjectAddress)
-        .then(r => r.json())
-        .then(result => { return result.primaryImage } )
-        .then(result => appendImgToPage(result))
+
+function createUrlArray(dataObject) {
+    let characterArray = [];
+    for (let item of dataObject) {
+        // characterArray.push(item.person.image.original)
+        characterArray.push(item)
+    }
+    // console.log(`URL Array: ${characterArray}`);
+    return characterArray;
 }
+
+async function fetchQueryImage(characterObject) {
+    console.log(characterObject)
+    const queryImage = await faceapi.fetchImage(characterObject.character.image.original);
+    // console.log(queryImage);
+    appendImgToPage(characterObject.character.image.original);
+    let euc = await compareFaces(referenceImage, queryImage)
+    // compareFaces(referenceImage, queryImage)
+    let characterCard = {
+        characterName: characterObject.character.name,
+        imgSrc: characterObject.character.image.original,
+        distance: euc
+    }
+    console.log(characterCard);
+    return characterCard;
+}
+
 
 function appendImgToPage(imgSrc) {
     let img = document.createElement('img');
@@ -24,24 +49,40 @@ function appendImgToPage(imgSrc) {
     document.body.appendChild(img);
 }
 
-getObjectArray(APIaddress)
-    .then(result => result.map(getImage))
+
+function getCastImages(castURL) {
+    fetch(castURL)
+        .then(r => r.json())
+        .then(result => createUrlArray(result))
+        .then(result => result.map(fetchQueryImage))
+}
 
 
-// ----------------------------------------------------------
+async function compareFaces(referenceImage, queryImage) {
+    const results = await faceapi.detectSingleFace(referenceImage).withFaceLandmarks().withFaceDescriptor()
+    // console.log(results)
 
-// Fetch Object Image URL and Append To Page
+    if (!results) {
+        console.log("no face")
+        return
+    }
+    
+    const singleResult = await faceapi.detectSingleFace(queryImage).withFaceLandmarks().withFaceDescriptor()
+    // console.log(singleResult)
+    const dist = faceapi.euclideanDistance(results.descriptor, singleResult.descriptor);
+    return dist;
+}
 
-// function fetchAnImage(data) {
-//     for(let item of data) {
-//         console.log(item);
-//     }
-// }
+loadModel()
+    .then(console.log(`Models loaded`))
+    .then(resultArray => {
+        getCastImages(theOfficeCast)   
+    })
+    .then(console.log)
 
-
-// fetchAnImage(data);
-
-
-
-
-
+// Promise.all([
+//     faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+//     faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+//     faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
+// ])
+//     .then(getCastImages(theOfficeCast))
