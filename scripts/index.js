e9b4d92251af4163da8413bc5118f8fa96700864
David Schneider <david.jay.schneider@gmail.com>
2019-12-15 21:31:50 -0500

    adds changes to fetching images from TV API

--- scripts/index.js
+++ scripts/index.js
@@ -1,88 +1,167 @@
+const imageUpload = document.getElementById('imageUpload');
+const dropdownMenu = document.getElementById('dropdown');
 
-const theOfficeCast = 'http://api.tvmaze.com/shows/526/cast';
-const gameOfThronesCast = 'http://api.tvmaze.com/shows/82/cast';
-const walkingDeadCast = 'http://api.tvmaze.com/shows/73/cast';
-const referenceImage = document.getElementById('referenceImage');
+const theOffice = 'http://api.tvmaze.com/shows/526/cast';
+const gameOfThrones = 'http://api.tvmaze.com/shows/82/cast';
+const walkingDead = 'http://api.tvmaze.com/shows/73/cast';
+const friends = 'http://api.tvmaze.com/shows/431/cast';
+const bigBang = 'http://api.tvmaze.com/shows/66/cast';
+const breakingBad = 'http://api.tvmaze.com/shows/169/cast';
+const doctorWho = 'http://api.tvmaze.com/shows/210/cast';
+const houseOfCards = 'http://api.tvmaze.com/shows/175/cast';
+const downtonAbbey = 'http://api.tvmaze.com/shows/251/cast';
+const ER = 'http://api.tvmaze.com/shows/547/cast';
+const lost = 'http://api.tvmaze.com/shows/123/cast';
+const thirtyRock = 'http://api.tvmaze.com/shows/537/cast';
+const satLive = 'http://api.tvmaze.com/shows/361/cast';
+const seinfeld = 'http://api.tvmaze.com/shows/530/cast';
+const sopranos = 'http://api.tvmaze.com/shows/527/cast';
 
-function loadModel() {
-    const mainModel = faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
-    const landmarkModel = faceapi.nets.faceLandmark68Net.loadFromUri('/models')
-    const recModel = faceapi.nets.faceRecognitionNet.loadFromUri('/models')
-    // await faceapi.nets.faceLandmark68TinyNet.loadFromUri('/models'),
-    // await faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-    return Promise.all([mainModel, landmarkModel, recModel])
+let showUrl = lost;
+
+dropdownMenu.addEventListener('click', (event) => {
+    console.log('Button has been clicked.')
+});
+
+
+
+
+Promise.all([
+    // faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68TinyNet.loadFromUri('/models'),
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models')
+])
+    .then(start)
+
+function start() {
+    document.body.append('API Models Loaded')
+    imageUpload.addEventListener('change', async function() {
+        const referenceImage = await faceapi.bufferToImage(imageUpload.files[0])
+        document.body.append(referenceImage)
+        try {
+            runComparison(showUrl);
+        } 
+        catch(err) {
+            console.log(`Error has occured. ${err}`)
+        }
+    })
 }
+
+
 
 
 function createUrlArray(dataObject) {
     let characterArray = [];
     for (let item of dataObject) {
-        // characterArray.push(item.person.image.original)
         characterArray.push(item)
     }
-    // console.log(`URL Array: ${characterArray}`);
     return characterArray;
 }
 
-async function fetchQueryImage(characterObject) {
-    console.log(characterObject)
-    const queryImage = await faceapi.fetchImage(characterObject.character.image.original);
-    // console.log(queryImage);
-    appendImgToPage(characterObject.character.image.original);
-    let euc = await compareFaces(referenceImage, queryImage)
-    // compareFaces(referenceImage, queryImage)
-    let characterCard = {
-        characterName: characterObject.character.name,
-        imgSrc: characterObject.character.image.original,
-        distance: euc
-    }
-    console.log(characterCard);
-    return characterCard;
+
+function getCalculations(characterArray) {
+    return Promise.all(
+        characterArray.map(fetchQueryImage)
+    )
 }
 
+async function fetchQueryImage(characterObject) {
+    try {
+        const referenceImage = await faceapi.bufferToImage(imageUpload.files[0])
+        // const queryImage = await faceapi.fetchImage(characterObject.character.image.original);
+        const queryImage = await faceapi.fetchImage(characterObject.person.image.original);
+        const calculation = await compareFaces(referenceImage, queryImage)
+        let characterCard = {
+            characterName: characterObject.character.name,
+            imgSrc: characterObject.person.image.original,
+            distance: calculation
+        }
+        console.log(characterCard);
+        return characterCard;
+    }
+    catch(err) {
+        console.log(`Error occured during fetchQueryImage. Error ${err}`);
+        const queryImage = await faceapi.fetchImage(characterObject.person.image.original);
+    }
+}
 
 function appendImgToPage(imgSrc) {
     let img = document.createElement('img');
     img.src = imgSrc;
-    img.width = "300";
-    img.height = "300";
     document.body.appendChild(img);
 }
 
+function appendMessageToPage(characterName) {
+    let h3 = document.createElement('h3');
+    h3.textContent = `Congrats! Your closest character match is ${characterName}.`;
+    document.body.appendChild(h3);
+}
 
-function getCastImages(castURL) {
-    fetch(castURL)
-        .then(r => r.json())
-        .then(result => createUrlArray(result))
-        .then(result => result.map(fetchQueryImage))
+function renderResultsToPage(characterCard) {
+    appendImgToPage(characterCard.imgSrc)
+    appendMessageToPage(characterCard.characterName)
+    // console.log(characterCard.distance)
+}
+
+
+async function runComparison(showUrl) {
+    try {
+        const response = await fetch(showUrl);
+        const myJson = await response.json();
+        const characterArray = await createUrlArray(myJson);
+        const objectArray = await getCalculations(characterArray);
+        const sortedArray = objectArray.sort((a, b) => (a.distance > b.distance) ? 1 : -1).filter(function (el) {
+            return el.distance != null;
+        });
+        console.log(sortedArray);
+        renderResultsToPage(sortedArray[0]);
+    }
+    catch(err) {
+        console.log('Error occured during runComparison.')
+    }
 }
 
 
 async function compareFaces(referenceImage, queryImage) {
-    const results = await faceapi.detectSingleFace(referenceImage).withFaceLandmarks().withFaceDescriptor()
-    // console.log(results)
-
-    if (!results) {
-        console.log("no face")
-        return
-    }
+    try {
+        const useTinyModel = true;
+        const results = await faceapi.detectSingleFace(referenceImage, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks(useTinyModel).withFaceDescriptor()
+        // console.log(results)
     
-    const singleResult = await faceapi.detectSingleFace(queryImage).withFaceLandmarks().withFaceDescriptor()
-    // console.log(singleResult)
-    const dist = faceapi.euclideanDistance(results.descriptor, singleResult.descriptor);
-    return dist;
+        if (!results) {
+            console.log("no face")
+            return
+        }
+        
+        const singleResult = await faceapi.detectSingleFace(queryImage, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks(useTinyModel).withFaceDescriptor()
+        // console.log(singleResult)
+        const dist = faceapi.euclideanDistance(results.descriptor, singleResult.descriptor);
+        // console.log(dist)
+        return dist;
+    }
+    catch(err) {
+        console.log("Couldn't calculate.")
+    }
 }
 
-loadModel()
-    .then(console.log(`Models loaded`))
-    .then(resultArray => {
-        getCastImages(theOfficeCast)   
-    })
-    .then(console.log)
 
-// Promise.all([
-//     faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-//     faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-//     faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
-// ])
-//     .then(getCastImages(theOfficeCast))
+// function getCastImages(castURL) {
+//     fetch(gameOfThronesCast)
+//         .then(r => r.json())
+//         .then(result => createUrlArray(result))
+//         .then(result => result.map(fetchQueryImage))
+//         // .then((result => result.sort((a, b) => (a.distance > b.distance) ? 1 : -1)))
+//         // .then(result => renderResultsToPage(result[0]))
+// }
+
+// function loadModel() {
+//     const mainModel = faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
+//     const landmarkModel = faceapi.nets.faceLandmark68Net.loadFromUri('/models')
+//     const recModel = faceapi.nets.faceRecognitionNet.loadFromUri('/models')
+//     const tinyLandmark = faceapi.nets.faceLandmark68TinyNet.loadFromUri('/models')
+//     const tinyFace = faceapi.nets.tinyFaceDetector.loadFromUri('/models')
+//     return Promise.all([mainModel, landmarkModel, recModel, tinyLandmark, tinyFace])
+// }
+
